83a8bacb562c92fdee721a77436039679af3273a
Gustavo ERNESTO <102641696+GusMaEr@users.noreply.github.com>
2024-07-08 15:12:05 -0300

    20240708_orden por carpetas

--- db.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE proyecto1_BackEnd;
-USE proyecto1_BackEnd;
-
-CREATE TABLE users (
-  id INT PRIMARY KEY AUTO_INCREMENT,
-  username VARCHAR(50) NOT NULL,
-  password VARCHAR(255) NOT NULL
-);
